5cf1a42b1fcf0c578aec6e3bef9502ecfaa0ea89
Paulien Lemay <paulien.lemay@cegeka.com>
2017-02-23 15:39:17 +0100

    orders

--- Order/src/main/java/be/cegeka/orders/order/domain/orders/Order.java
+++ Order/src/main/java/be/cegeka/orders/order/domain/orders/Order.java
@@ -1,12 +1,25 @@
 package be.cegeka.orders.order.domain.orders;
 
-import javax.inject.Named;
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 /**
  * Created by paulienl on 23/02/2017.
  */
 @Entity
-@Named
+@Table(name="ORDERS")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ORDER_ID")
+    private String id;
+
+    @Column(name="ORDER_DATE")
+    private String orderDate;
+
+    public Order(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Order() {
+    }
 }
