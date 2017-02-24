455b455553257ce42fbf171cbd7ac1e6261b679f
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-24 11:58:14 +0100

    Shipment fixed

--- Order/src/main/java/be/cegeka/orders/order/domain/orders/Order.java
+++ Order/src/main/java/be/cegeka/orders/order/domain/orders/Order.java
@@ -1,7 +1,11 @@
 package be.cegeka.orders.order.domain.orders;
 
+import be.cegeka.orders.order.domain.customers.Customer;
+import be.cegeka.orders.order.domain.shipment.Shipment;
+
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Created by paulienl on 23/02/2017.
@@ -17,14 +21,20 @@ public class Order {
     @Column(name="ORDER_DATE")
     private LocalDate orderDate;
 
-//    @Column(name="CUSTOMER_ID")
-//    private int customerId;
+    @ManyToOne
+    @JoinColumn(name="CUSTOMER_ID")
+    private Customer customerId;
 
 
-    public Order(LocalDate orderDate) {
-        this.orderDate = orderDate;
-    }
+    @OneToMany
+    @JoinColumn(name = "ORDER_ID")
+    private List<Shipment> shipments;
 
     public Order() {
+    }
+
+    public Order(LocalDate orderDate, Customer customerId ) {
+        this.orderDate = orderDate;
+        this.customerId = customerId;
     }
 }
--- Order/src/main/java/be/cegeka/orders/order/domain/shipment/Shipment.java
+++ Order/src/main/java/be/cegeka/orders/order/domain/shipment/Shipment.java
@@ -1,4 +1,4 @@
-package be.cegeka.orders.order.domain.itemsOnOrder;
+package be.cegeka.orders.order.domain.shipment;
 
 import be.cegeka.orders.order.domain.items.Item;
 import be.cegeka.orders.order.domain.orders.Order;
@@ -10,29 +10,24 @@ import java.time.LocalDate;
  * Created by roelg on 24/02/2017.
  */
 @Entity
-@Table(name = "ITEMS_ON_ORDERS")
-public class ItemOnOrder {
+@Table(name = "SHIPMENTS")
+public class Shipment {
     @Id
-    @Column(name = "IOO_ID")
+    @Column(name = "SHIPMENT_ID")
     int ioo;
 
     @ManyToOne
     @JoinColumn(name = "ITEM_ID")
     Item item;
 
-    @ManyToOne
-    @JoinColumn(name = "ORDER_ID")
-    Order order;
-
     @Column(name = "SHIPPING_DATE")
     LocalDate shipping_date;
 
-    public ItemOnOrder() {
+    public Shipment() {
     }
 
-    public ItemOnOrder(Item item, Order order, LocalDate shipping_date) {
+    public Shipment(Item item, LocalDate shipping_date) {
         this.item = item;
-        this.order = order;
         this.shipping_date = shipping_date;
     }
 }
