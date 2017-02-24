0224ea1e04a6f1a8eea2118b11b116331d336174
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-02-24 14:20:35 +0100

    stockrepository test

--- Order/src/main/java/be/cegeka/orders/order/domain/stock/StockRepository.java
+++ Order/src/main/java/be/cegeka/orders/order/domain/stock/StockRepository.java
@@ -15,6 +15,10 @@ public class StockRepository {
     private EntityManager entityManager;
 
     public List<StockEntry> getStock() {
-        return entityManager.createQuery("select c from stock c", StockEntry.class).getResultList();
+        return entityManager.createQuery("select s from StockEntry s", StockEntry.class).getResultList();
+    }
+
+    public void addEntry(StockEntry stock) {
+        entityManager.persist(stock);
     }
 }
--- Order/src/test/java/be/cegeka/orders/order/domain/stock/StockRepositoryTest.java
+++ Order/src/test/java/be/cegeka/orders/order/domain/stock/StockRepositoryTest.java
@@ -3,6 +3,7 @@ package be.cegeka.orders.order.domain.stock;
 import be.cegeka.orders.order.OrderApplication;
 import be.cegeka.orders.order.domain.items.Item;
 import org.assertj.core.api.Assertions;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,8 +33,12 @@ public class StockRepositoryTest {
 
     @Before
     public void setUp(){
-        stock1 = new StockEntry(new Item("Iets", "dit doet iets", 1235));
-        stock2 = new StockEntry(new Item("Iets anders", "dit doet iets anders", 12356));
+        Item item1 = new Item("Iets", "dit doet iets", 1235);
+        Item item2= new Item("Iets anders", "dit doet iets anders", 12356);
+        entityManager.persist(item1);
+        entityManager.persist(item2);
+        stock1 = new StockEntry(item1);
+        stock2 = new StockEntry(item2);
         entityManager.persist(stock1);
         entityManager.persist(stock2);
     }
@@ -43,11 +48,17 @@ public class StockRepositoryTest {
         Assertions.assertThat(stockRepository.getStock()).contains(stock1, stock2);
     }
 
-//    @Test
-//    public void addCustomerShouldAddCustomer(){
-//        StockEntry stock= new StockEntry(new Item("Iets", "dit doet iets", 1235));
-//        stockRepository.addEntry(stock);
-//        Assertions.assertThat(stockRepository.getAll()).contains(stock1, stock2, stock);
-//    }
+    @Test
+    public void addStockEntryShouldAddEntry(){
+        Item item3=new Item("pinkfluffyunicorn", "it's so fluffy", 99999.00);
+        entityManager.persist(item3);
+        StockEntry stock= new StockEntry(item3);
+        stockRepository.addEntry(stock);
+        Assertions.assertThat(stockRepository.getStock()).contains(stock1, stock2, stock);
+    }
+    @After
+    public void cleanDatabase() {
+        entityManager.clear();
+    }
 
 }
