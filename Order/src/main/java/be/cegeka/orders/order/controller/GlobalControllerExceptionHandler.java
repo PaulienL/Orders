dbd05afaa956c32c9c3c32bb8d61cb67643a6958
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-27 12:45:34 +0100

    Added Controller Advisor

--- Order/src/main/java/be/cegeka/orders/order/controller/GlobalControllerExceptionHandler.java
+++ Order/src/main/java/be/cegeka/orders/order/controller/GlobalControllerExceptionHandler.java
@@ -1,7 +1,27 @@
 package be.cegeka.orders.order.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Created by roelg on 27/02/2017.
  */
+@ControllerAdvice
 public class GlobalControllerExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
+    @ResponseBody
+    public String handleIllegalArgument(Exception ex) {
+        return ex.getMessage();
+    }
+
 }
