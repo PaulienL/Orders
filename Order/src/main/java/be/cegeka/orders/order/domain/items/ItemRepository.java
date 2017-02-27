c1fadaa15ab3182815f8d7feae2e4dc8be7b5053
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-27 10:41:56 +0100

    GVD, repo and service, Service has some valitdation. Fuck you sourcetree

---
+++ Order/src/main/java/be/cegeka/orders/order/domain/items/ItemRepository.java
@@ -0,0 +1,26 @@
+package be.cegeka.orders.order.domain.items;
+
+
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * Created by roelg on 27/02/2017.
+ */
+@Named
+public class ItemRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void addItem(Item item) {
+        entityManager.persist(item);
+    }
+
+    public List<Item> getItems() {
+        return entityManager.createQuery("select o from Item o" , Item.class).getResultList();
+    }
+
+}
